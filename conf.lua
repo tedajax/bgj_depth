c0093986580ffe6ee1586d9778256c65a36887ac
Ted Dobyns <ted.ajax@gmail.com>
2015-06-14 17:32:30 -0700

    updated config so the score system actually works

--- conf.lua
+++ conf.lua
@@ -1,7 +1,8 @@
 function love.conf(t)
-    t.identify = "nuclear_blimp"
+    t.identity = "nuclear_blimp"
     t.version = "0.9.2"
     t.console = false
+    t.release = true
 
     t.window.title = "Nuclear Blimp"
     t.window.icon = "nuclear_blimp.ico"
